2e0de724ba86729eb572e473f0698f83fc74970f
Rikuto <mikadorikuto@gmail.com>
2026-01-22 23:02:03 +0900

    update the code

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
-  <section></section>
+  <div>
+    <active-user></active-user>
+    <user-data></user-data>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style>
+section {
+    margin: 2rem auto;
+    max-width: 40rem;
+    border-radius: 12px;
+    border: 1px solid #ccc;
+}
+</style>
--- src/components/ActiveUser.vue
+++ src/components/ActiveUser.vue
@@ -8,12 +8,3 @@
 <script>
 export default {};
 </script>
-
-<style>
-section {
-    margin: 2rem auto;
-    max-width: 40rem;
-    border-radius: 12px;
-    border: 1px solid #ccc;
-}
-</style>
