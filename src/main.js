77ca73ed3a2c54054b552a2ea835ea6ad3b753a1
Rikuto <mikadorikuto@gmail.com>
2026-01-22 15:25:15 +0900

    update the code

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import { createApp } from 'vue'
 
+import ActiveUser from './components/ActiveUser.vue'
+import UserData from './components/UserData.vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.component('ActiveUser', ActiveUser);
+app.component('UserData', UserData);
+
+app.mount('#app');
+
 
 // Task 1: 
 // Add two components to the app: 
