3e20da77ef5c03b66a1668053e3a2294e3f69080
Rikuto <mikadorikuto@gmail.com>
2026-01-25 23:00:55 +0900

    update the code

--- src/components/UserData.vue
+++ src/components/UserData.vue
@@ -1,10 +1,32 @@
 <template>
   <section>
-    <input type="text" placeholder="Name" />
-    <input type="text" placeholder="Age" />
+    <form @submit.prevent="submitData">
+      <!-- v-model: two-way binding (syncs input value with enteredName) -->
+      <input type="text" placeholder="Name" v-model="enteredName" />
+      <input type="text" placeholder="Age" v-model="enteredAge" />
+      <button>Set User Data</button>
+    </form>
   </section>
 </template>
 
 <script>
-export default {};
+export default {
+  // emits: declares custom events this component can emit to parent
+  emits: ['set-data'],
+  data() {
+    return {
+      enteredName: '',
+      enteredAge: ''
+    }
+  },
+  methods: {
+    submitData() {
+      // $emit: sends custom event 'userdata' with payload to parent component    
+      this.$emit('set-data', {
+        name: this.enteredName,
+        age: this.enteredAge
+      })
+    }
+  }
+};
 </script>
